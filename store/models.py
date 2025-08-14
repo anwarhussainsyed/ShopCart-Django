fd3508f577e9a38ccb3e03c64934c5358305d240
Anwar Hussain <syedanwar9362@gmail.com>
2025-08-14 14:42:23 +0530

    Store and Carts Functionality

--- store/models.py
+++ store/models.py
@@ -19,3 +19,20 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+    
+class VartiationManager(models.Manager):
+    def colors(self):
+        return super(VartiationManager,self).filter(variation_category='color',is_active=True)
+    def sizes(self):
+        return super(VartiationManager,self).filter(variation_category='size',is_active=True)
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=(('color', 'color'), ('size', 'size')), default='color')
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    objects = VartiationManager()
+
+    def __str__(self):
+        return self.variation_value
+    
